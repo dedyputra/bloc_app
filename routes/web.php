ff8e9874e03543b3b71e1c172cc30017c4dfea7a
Dedy Putra Aditia <73837147+dedyputra@users.noreply.github.com>
2024-07-12 11:32:18 +0700

    create views, routes

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('Home');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+
+Route::get('/blog', function () {
+    return view('blog');
 });
